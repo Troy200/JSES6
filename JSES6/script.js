8f395c470e25aa463a03fee4a632797f1879ea76
Troy200 <kuriyanthomas27@gmail.com>
2026-04-01 18:00:46 +0200

    Add files via upload

---
+++ JSES6/script.js
@@ -0,0 +1,8 @@
+let ok=()=>{
+let n= document.getElementById("name").value
+document.getElementById("msg").innerHTML="Happy Easter " + n
+document.getElementById("heimg").src="https://bielyshoaf.com/cdn/shop/files/LME001_1200x1200.jpg?v=1703806316"
+
+}
+
+document.getElementById("okb").addEventListener("click",ok)
