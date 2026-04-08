f8dd9d63a0d00f0c6bd736a141cd37dcfaef4eb6
Troy200 <kuriyanthomas27@gmail.com>
2026-04-08 17:26:49 +0200

    Add files via upload

--- JSES6/script.js
+++ JSES6/script.js
@@ -1,8 +1,25 @@
-let ok=()=>{
+let ebutton=()=>{
 let n= document.getElementById("name").value
 document.getElementById("msg").innerHTML="Happy Easter " + n
 document.getElementById("heimg").src="https://bielyshoaf.com/cdn/shop/files/LME001_1200x1200.jpg?v=1703806316"
-
+document.getElementById("cssedit").style.backgroundColor="#fff7f0"
+document.getElementById("heimg").style.visibility="visible"
 }
 
-document.getElementById("okb").addEventListener("click",ok)
+document.getElementById("easterb").addEventListener("click",ebutton)
+
+
+
+
+let bbutton=()=>{
+    let n= document.getElementById("name").value
+    document.getElementById("msg").innerHTML="Happy Birthday " + n
+    document.getElementById("heimg").src="https://ecards.hopespring.org.uk/wp-content/uploads/2021/05/classy-happy-birthday-eCard.jpg"
+
+    document.getElementById("cssedit").style.backgroundColor="red"
+    document.getElementById("heimg").style.visibility="visible"
+    
+    }
+
+
+    document.getElementById("birthdayb").addEventListener("click",bbutton)
